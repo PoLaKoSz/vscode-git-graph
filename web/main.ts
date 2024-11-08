30a36b3dd156df8a665990c9bfb791804cb067db
Tom PoLáKoSz <polakosz@freemail.hu>
2024-11-08 11:45:32 +0100

    Fork like branch management

--- web/main.ts
+++ web/main.ts
@@ -2612,7 +2612,7 @@ class GitGraphView {
 		this.branchFilter = this.branchFilterElem.value;
 		const branches = Array.from(document.querySelectorAll('#branch-tree li')!);
 		for (const branch of branches) {
-			branch.classList.toggle('d-none', !branch.textContent?.includes(this.branchFilter));
+			branch.classList.toggle('d-none', !branch.textContent?.toLowerCase().includes(this.branchFilter.toLowerCase()));
 		}
 	}
 
